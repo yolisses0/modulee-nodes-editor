9f11eeede5d37ed5dace97a8fc14958bb8ebd27b
Yolisses <yolisses0@gmail.com>
2025-01-19 19:39:24 -0300

    Fix NodeItem props

--- src/lib/NodeItem.svelte
+++ src/lib/NodeItem.svelte
@@ -1,11 +1,14 @@
-<script>
+<script lang="ts">
 	import { NodeItem } from 'nodes-editor';
+	import type { Node } from './data/Node.svelte.js';
 
 	interface Props {
 		node: Node;
 	}
+
+	const { node }: Props = $props();
 </script>
 
-<NodeItem>
+<NodeItem {node}>
 	<div class="bg-zinc-800"></div>
 </NodeItem>
