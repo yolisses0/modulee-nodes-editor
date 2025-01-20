f1c597740775398b4f9c22966846ba62bf6d20bc
Yolisses <yolisses0@gmail.com>
2025-01-20 17:54:38 -0300

    Create custom WirePath

--- src/lib/connection/Wire.svelte
+++ src/lib/connection/Wire.svelte
@@ -1,12 +1,12 @@
 <script lang="ts">
 	import { Vector } from '$lib/space/Vector.js';
-	import { WirePath, WireSvg } from 'nodes-editor';
+	import { WireSvg } from 'nodes-editor';
+	import WirePath from './WirePath.svelte';
 	import type { WireProps } from './WireProps.js';
-	import { getPathD } from './getPathD.js';
 
 	const { endPosition, startPosition }: WireProps = $props();
 </script>
 
 <WireSvg {startPosition} {endPosition} margin={new Vector(10, 10)}>
-	<WirePath {getPathD} {startPosition} {endPosition} />
+	<WirePath {startPosition} {endPosition} />
 </WireSvg>
---
+++ src/lib/connection/WirePath.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { getPathD } from './getPathD.js';
+	import type { WireProps } from './WireProps.js';
+
+	const { endPosition, startPosition }: WireProps = $props();
+
+	const pathD = $derived(getPathD(startPosition, endPosition));
+</script>
+
+<path d={pathD} stroke="#14532d" fill="transparent" stroke-width="0.25lh" />
+<path d={pathD} stroke="#22c55e" fill="transparent" stroke-width="0.15lh" stroke-linecap="round" />
