4941ccb9a9e3b5343afc42e7d2599148e465d184
Yolisses <yolisses0@gmail.com>
2025-01-22 16:35:21 -0300

    Create overlay for closing AddNodeMenu

--- src/lib/node/AddNodeMenuWrapper.svelte
+++ src/lib/node/AddNodeMenuWrapper.svelte
@@ -6,15 +6,20 @@
 		mouseEvent?: MouseEvent;
 	}
 
-	const { mouseEvent }: Props = $props();
+	let { mouseEvent = $bindable() }: Props = $props();
 	const nodeListContext = getNodeListContext();
 	const menuPosition = $derived.by(() => {
 		if (!mouseEvent) return;
 		if (!nodeListContext.nodeList) return;
 		return getMouseRelativePosition(mouseEvent, nodeListContext.nodeList);
 	});
+
+	function handleOverlayClick() {
+		mouseEvent = undefined;
+	}
 </script>
 
 {#if menuPosition}
+	<button onclick={handleOverlayClick} class="absolute h-full w-full" aria-label="overlay"></button>
 	<AddNodeMenu screenPosition={menuPosition}></AddNodeMenu>
 {/if}
