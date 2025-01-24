7d826a26f7e3c523c8d520d2590cff894e8d1ef1
Yolisses <yolisses0@gmail.com>
2025-01-24 14:57:01 -0300

    Export instantiateCommand

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -3,11 +3,11 @@ import { Node } from './data/Node.svelte.js';
 import type { NodeData } from './data/NodeData.js';
 import type { CommandData } from './editor/CommandData.js';
 import { Editor } from './editor/Editor.svelte.js';
+import { instantiateCommand } from './editor/instantiateCommand.js';
 
 import NodesPage from '$lib/node/NodesPage.svelte';
 
 // TODO check if it makes sense to export NodeData
 
-export { Editor, Node, NodesPage };
-
+export { Editor, instantiateCommand, Node, NodesPage };
 export type { CommandData, EditorData, NodeData };
