84a4e9b425f540a8235575a481505312b708f514
Yolisses <yolisses0@gmail.com>
2025-01-24 16:53:43 -0300

    Fix instantiateCommand

--- src/lib/editor/commandClasses.ts
+++ src/lib/editor/commandClasses.ts
@@ -1,11 +1,13 @@
 import { AddNodeCommand } from '$lib/commands/AddNodeCommand.js';
 import { MoveNodeCommand } from '$lib/commands/MoveNodeCommand.js';
 import { RemoveNodeCommand } from '$lib/commands/RemoveNodeCommand.js';
+import { SetInputConnectedOutput } from '$lib/commands/SetInputConnectedOutput.js';
 import type { CommandClass } from './CommandClass.js';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const commandClasses: CommandClass<any>[] = [
 	AddNodeCommand,
-	RemoveNodeCommand,
 	MoveNodeCommand,
+	RemoveNodeCommand,
+	SetInputConnectedOutput,
 ];
--- src/lib/editor/instantiateCommand.ts
+++ src/lib/editor/instantiateCommand.ts
@@ -4,5 +4,5 @@ import type { CommandData } from './CommandData.js';
 export function instantiateCommand(commandData: CommandData) {
 	const constructor = commandClassesByType[commandData.type];
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	return new constructor(commandData.details as any);
+	return new constructor(commandData as any);
 }
